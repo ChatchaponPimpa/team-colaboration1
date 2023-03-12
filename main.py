cb8ad02a41aef52de157b207a682f7d43d7d6058
MrBoy <boy123456007@gmail.com>
2023-03-12 14:21:33 +0700

    boy

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 import re
+import long_responses as long
+
+def get_response(user_input):
+    split_message = re.split(r'\s+l[,;?!.-]\s-*',user_input.lower())
+
+# Testing the response system
+while True:
+    print("Bot:" + get_response(input("You":)))
