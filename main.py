8908f538445b1e8b768b3ba8fc7d3c1d27dab4e7
MrBoy <boy123456007@gmail.com>
2023-03-12 16:32:46 +0700

    39 46 51 กำลังแก้ไขอยู่

--- main.py
+++ main.py
@@ -25,21 +25,21 @@ def message_probability (user_message , recognised_words, single_response=False,
         return 0
 
 def check_all_messages (message):
-    higest_prob_list = {}
+    highest_prob_list = {}
 
     def response (bot_response, list_of_words, single_response=False, required_words=[]):
-        nonlocal higest_prob_list
-        higest_prob_list [bot_response] = message_probability (message, list_of_words, single_response, required_words)
+        nonlocal highest_prob_list
+        highest_prob_list [bot_response] = message_probability (message, list_of_words, single_response, required_words)
 
     # Response ----------------------------------------             
     response ('Hello!', ['hello', 'hi', 'sup', 'hey', 'heyo'], single_response=True)
     response ('I\'m doing fine, and you?', ['how', 'are', 'you', 'doing'], required_words=['how'])
     response ('Thamk you!', ['i', 'love', 'code', 'palace'], required_words=['ciode'])
 
-    best_match = max (higest_prob_list, key=higest_prob_list.get())
-    print (higest_prob_list)
+    best_match = max (highest_prob_list, key = highest_prob_list.get())
+    print (highest_prob_list)
 
-    return long.unknown() if higest_prob_list [best_match] < 1 else best_match
+    return long.unknown() if highest_prob_list [best_match] < 1 else best_match
 
 def get_response (user_input):
     split_message = re.split (r'\s+l[,;?!.-]\s*', user_input.lower())
@@ -48,4 +48,4 @@ def get_response (user_input):
 
 # Testing the response system
 while True:
-    print('Bot: ' + get_response(input('You: ')))
+    print ('Bot: ' + get_response (input('You: ')))
