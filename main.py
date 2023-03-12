6e23a220ebdd9277a2eea17aed2caa554ea7d64f
MrBoy <boy123456007@gmail.com>
2023-03-12 14:27:01 +0700

    boy

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import re
 import long_responses as long
 
+daf message_probab
+
 def get_response(user_input):
     split_message = re.split(r'\s+l[,;?!.-]\s-*',user_input.lower())
+    response = check_all_messages(split_message)
+    return response
 
 # Testing the response system
 while True:
-    print("Bot:" + get_response(input("You":)))
+    print('Bot: ' + get_response(input('You: ')))
