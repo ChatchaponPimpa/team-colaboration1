48c90f43a2d6a141cca6b8654f6d3617a2e81029
MrBoy <boy123456007@gmail.com>
2023-03-12 15:15:30 +0700

    เสร็จ

---
+++ long_responses.py
@@ -0,0 +1,7 @@
+import rendom
+
+R_EATING = "I don't like eating anything becayse I'm bot obviously!"
+
+def unknown():
+    response = ['Could you please re-phrase that?', "...", "Sounds about right", "whet does that mean?"][random.randrange(4)]
+    return response
